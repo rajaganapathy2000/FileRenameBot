84c0f0f92a2d7fa1c1ede396766042949d84db67
rajaganapathy2000 <79167443+rajaganapathy2000@users.noreply.github.com>
2021-03-02 23:02:21 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer and File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
 
 <b>Bot Maintained By: @SeriesDiscussionForum </b> \n 
 <b> I Can Also Download/Upload Files From Zee5</b> \n
@@ -9,30 +9,30 @@ class Translation(object):
     RENAME_403_ERR = "What Are You Doing? Bro!!"
     BANNED_USER_TEXT = "Sorry!! But My Owner As Banned You From Using This Service. That Means You Can't Run Me Now! \n \n Contact : @Rajaganapathy2000 For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @Rajaganapathy2000 "
-    DOWNLOAD_START = "<b>Downloading To My server !! Please Wait</b>"
-    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into Telegram</b>"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me SHARE > ❤️\n Join and Support us @SeriesDiscussionForum**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @Rajaganapathy2000 "
+    UPGRADE_TEXT = "Contact @Rajaganapathy2000 "
+    DOWNLOAD_START = "<b>Downloading To My server 📥 !! Please Wait🥴</b>"
+    UPLOAD_START = "<b>Downloading Completed 📥✅ Now I'm Uploading Into Telegram 📤</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?😵"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me SHARE > ❤️\n Join and Support us @SeriesDiscussionForum 🥰**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds 🥳.\nUploaded in {} seconds 🥳"
+    NOT_AUTH_USER_TEXT = "Contact @Rajaganapathy2000 "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @Rajaganapathy2000 "
-    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅\nThis Is Permanent Until You Type</b> /delthumb ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared ❌ Succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media Cleared ❌ Succesfully🤦."
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded 📥✅ Successfully 😎🥳</b>"
     REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
     REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
     REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
     CUSTOM_CAPTION_UL_FILE = " @SeriesDiscussionForum "
-    NO_THUMB_FOUND = "No Thumbnail found"
+    NO_THUMB_FOUND = "No Thumbnail Found 🤧"
     FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>⬇️</a>
 🎞  - Stream format
 📁  - File format
 <i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 😇
 """
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters🤬😡"""
     ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n I can also convert files into videos \n Feedback @Rajaganapathy2000 If You Found Me Useful</b>"""
     HELP_USER = """Ohh You Want Help?? 😅
     
@@ -40,5 +40,5 @@ class Translation(object):
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
 3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
 4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n
-5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: @SeriesDiscussionForum </b>"""
+5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Recreated By: @SeriesDiscussionForum \n Credits to @MaI_BoTs</b>"""
 
